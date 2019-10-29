fdcdce35eb2e68cf261c902beddbe128286feda8
AMP-3-PC\AMP-3 <mrianfathany32@gmail.com>
2019-10-29 10:43:34 +0700

    update game

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './assets/css/font/font-awesome/css/font-awesome.css';
 import './assets/css/font/fontStyle.css';
 import './assets/css/index.css';
+import './assets/css/hover.css';
 
 
 ReactDOM.render(<App/>,document.getElementById("app"));
